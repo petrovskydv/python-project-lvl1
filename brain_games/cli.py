020bbfd532f0a43995e7bdcc0b630d8082c56cc4
denis <petrovsky.dv@gmail.com>
2022-05-07 18:09:17 +0300

    step 3

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt
+
+
+def get_name():
+    return prompt.string('May I have your name? ')
---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from brain_games.cli import get_name
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = get_name()
+    print(f'Hello, {name}!')
+
+
+if __name__ == '__main__':
+    main()
