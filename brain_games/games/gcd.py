0e7536f99f120b85d420bdd2703b378b91458147
denis <petrovsky.dv@gmail.com>
2022-05-08 19:26:46 +0300

    step 7

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,22 @@
+import random
+
+
+def get_question():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    return number1, number2
+
+
+def get_answer(numbers):
+    number1, number2 = numbers
+    while number1 != 0 and number2 != 0:
+        if number1 > number2:
+            number1 %= number2
+        else:
+            number2 %= number1
+
+    return str(number1 + number2)
+
+
+def get_game_description():
+    return 'Find the greatest common divisor of given numbers.'
